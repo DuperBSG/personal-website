b35d2e402aa3e1a1f17c73fe5aab381ba2f9a060
DuperBSG <120407043+DuperBSG@users.noreply.github.com>
2024-07-21 17:18:03 +0800

    First Update
    Some Basic title

---
+++ src/JSXComponents/Introduction.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../Styles/Introduction.css'
+
+export default function Introduction() {
+    return (
+        <div className='introduction-container'>
+            <p>THIS IS SOME RANDOM TEXT </p>
+            <p>IT WILL BE REPLACED LATER WITH EDITS </p>
+                
+            <p>HERE ARE MORE THINGS</p>
+
+            <p> - HI</p>
+            <p> - YOOOOOO</p>
+            <p> - Thats import PropTypes from 'prop-types'</p>
+                
+            <p>HERE IS A SHORT CONCLUSION</p>
+            <p>Thanks for watching lol</p>
+        </div>
+        
+    )
+}
---
+++ src/JSXComponents/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/Title.css'
+
+export default function Title() {
+    return (
+        <div className='title-container'>
+            <h1 className='title-text'>
+                Creativity
+            </h1>
+        </div>
+    )
+}
